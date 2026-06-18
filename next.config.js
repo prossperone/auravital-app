1c391f0fffbd4edfe200c7649cb55c173ff7a536
prossperone <tomvasquez@gmail.com>
2026-06-18 18:17:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'standalone',
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   webpack: (config) => {
     config.resolve.alias['@'] = __dirname;
     return config;
   },
 }
-
 module.exports = nextConfig
